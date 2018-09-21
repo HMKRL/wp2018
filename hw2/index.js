2376641db47c0af1f4b73cc278174ce4ea962fc3
You-Cheng Liang <liangyc39@gmail.com>
2018-09-22 00:19:11 +0800

    Finished hw2 & hw3

---
+++ hw2/index.js
@@ -0,0 +1,52 @@
+var current_card;
+function whichTransitionEvent(){
+    var t;
+    var el = document.createElement('fakeelement');
+    var transitions = {
+        'transition':'transitionend',
+        'OTransition':'oTransitionEnd',
+        'MozTransition':'transitionend',
+        'WebkitTransition':'webkitTransitionEnd'
+    }
+
+    for(t in transitions){
+        if( el.style[t] !== undefined ){
+            return transitions[t];
+        }
+    }
+}
+function funcLoad() {
+    document.getElementById('delete').style.visibility = 'hidden';
+    var cards = document.getElementsByClassName('card');
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].onclick = function() { cardClicked(i); }
+        setTimeout(function(cards, i) { cards[i].style.top = i * 50 + 'px'; }, 120 * i, cards, i)
+    }
+    document.addEventListener(whichTransitionEvent(), function() {
+        setTimeout(function() { document.getElementById('delete').style.visibility = 'visible'; }, 400);
+    }, false);
+
+    current_card = cards[cards.length -1].id;
+}
+
+function cardClicked(cardnum) {
+    document.getElementById('delete').style.visibility = 'hidden';
+    var cards = document.getElementsByClassName('card');
+    for (let i = cards.length - 1; i > cardnum; i--) {
+        setTimeout(function(cards, i) { cards[i].style.top = 88 + i + '%'; }, 120 * (cards.length - 1 - i), cards, i)
+    }
+    for (let i = cardnum; i >= 0; i--) {
+        setTimeout(function(cards, i) { cards[i].style.top = i * 50 + 'px'; }, 120 * i , cards, i)
+    }
+
+    current_card = cards[cardnum].id;
+}
+
+function deleteCard() {
+    document.getElementById(current_card).style.opacity = '0';
+    setTimeout(function() {
+        document.getElementById(current_card).remove();
+        funcLoad();
+    }, 400);
+    
+}
